ba5bf4b70c28a98060b24a600065782307115f23
stanleyedward <stanleyedward9715@gmail.com>
2024-03-09 11:12:36 +0530

    finish ray h file

--- ray_proj.h
+++ ray_proj.h
@@ -1,9 +1,16 @@
 #ifndef ray_proj_h
 #define ray_proj_h
+
 #include <stdio.h>
 #include <glm/glm.hpp>
 using namespace glm;
 
 class Ray{
- 
-}
+    public:
+        glm::vec3 p0, p1;
+        float alpha, beta;
+        Ray(float alphaInput, float betaInput, glm::vec3 p0, glm::vec3 p1);
+        
+};
+
+#endif
