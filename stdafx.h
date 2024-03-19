2c9dd5f825546be69c1bdf2df256fb63ed15d6e8
stanleyedward <stanleyedward9715@gmail.com>
2024-03-19 23:33:27 +0530

    add standard includes file

---
+++ stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "glm/glm.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <stack>
+#include <vector>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+
+using namespace glm;
+using namespace std;
