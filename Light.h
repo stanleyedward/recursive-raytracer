7dc184d02c1217402059996498a6b6b119aee340
stanleyedward <stanleyedward9715@gmail.com>
2024-03-20 03:02:52 +0530

    def light direction and position

---
+++ Light.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum class LightType{ direction, point};
+class Light{
+    public:
+        float x, y, z;
+        float r, g, b;
+        LightType type;
+
+    public:
+        Light() = default;
+        Light(float x, float y, float z, float r, float g, float b, LightType type) : x(x), y(y), z(z), r(r), g(g), b(b), type(type) {}
+    
+};
