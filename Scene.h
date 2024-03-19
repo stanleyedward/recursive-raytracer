d5eeaf955e6e58fea2e2778d2bcb22063c2751bc
stanleyedward <stanleyedward9715@gmail.com>
2024-03-20 03:10:39 +0530

    compute scene

--- Scene.h
+++ Scene.h
@@ -1,2 +1,39 @@
 #pragma once
 #include "stdafx.h"
+#include "Object.h"
+#include "Light.h"
+#include "Camera.h"
+#include "Transform.h"
+
+static enum {view, translate, scale} transop;
+
+class Scene{
+    public:
+        int width = 640;
+        int height = 460;
+        float aspectRatio;
+
+        std::string output_filename = "output.png";
+
+        int maxdepth = 5;
+        int currentdepth = 0;
+
+        const static int numLights = 10;
+        Light* lights = new Light[numLights];
+        int numused;
+
+        Camera mainCamera;
+
+        const static int maxobjects = 100000;
+        int numobjects;
+        Object* objects[maxobjects];
+    
+    public:
+        Scene(){}
+        ~Scene(){
+            for(int i = 0; i < numobjects; i++){
+                delete objects[i];
+            }
+            delete[] lights;
+        }
+};
