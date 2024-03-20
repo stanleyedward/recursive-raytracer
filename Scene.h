1f4c6f483c28f8a73ee3baa1cc3c0966342f080d
stanleyedward <stanleyedward9715@gmail.com>
2024-03-20 11:19:44 +0530

    formatting

--- Scene.h
+++ Scene.h
@@ -1,40 +1,42 @@
 #pragma once
+#include "Transform.h" 
+#include "Object.h"
 #include "stdafx.h"
 #include "Object.h"
 #include "Light.h"
 #include "Camera.h"
-#include "Transform.h"
+
 
 static enum {view, translate, scale} transop;
 
-class Scene{
-    public:
-        int width = 640;
-        int height = 460;
-        float aspectRatio;
-        vec3 attenuation = vec3(1, 0, 0);
+class Scene {
+	public:
+		int width=640;
+		int height=460;
+		float aspectRatio;
+		vec3 attenuation = vec3(1, 0, 0);
 
-        std::string output_filename = "output.png";
+		std::string output_filename = "output.png";
 
-        int maxdepth = 5;
-        int currentdepth = 0;
+		int maxdepth = 5; 
+		int currentdepth = 0;
 
-        const static int numLights = 10;
-        Light* lights = new Light[numLights];
-        int numused;
+		const static int numLights = 10;
+		Light* lights = new Light[numLights];
+		int numused;  
+		Camera mainCamera; 
 
-        Camera mainCamera;
-
-        const static int maxobjects = 100000;
-        int numobjects;
-        Object* objects[maxobjects];
-    
-    public:
-        Scene(){}
-        ~Scene(){
-            for(int i = 0; i < numobjects; i++){
-                delete objects[i];
-            }
-            delete[] lights;
-        }
+		const static int maxobjects = 100000;
+		int numobjects;
+		Object* objects[maxobjects];
+	
+	public:
+		Scene() {}
+		~Scene() {
+			for (int i = 0; i < numobjects; i++) {
+				delete objects[i];
+			}
+			delete[] lights;
+		}
 };
+
